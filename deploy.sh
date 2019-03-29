839f408f1a84a9c7887e8096061c27459a7ed6af
Philipp Jardas <philipp.jardas@schuetze.ag>
2019-03-29 12:30:02 +0100

    Add deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,24 @@
-#!/bin/bash -e
+#!/bin/sh -e
+
+cd $(dirname "$0")
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
 hugo
-cd public && git add --all && git commit -m "Publishing to gh-pages" && cd ..
+
+echo "Updating gh-pages branch"
+cd public
+git add --all
+git commit -m "Publishing to gh-pages (publish.sh)"
+git push -u origin gh-pages
