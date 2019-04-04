c340185445cfd309948058f4e830aa0a231d306d
Philipp Jardas <philipp.jardas@schuetze.ag>
2019-04-04 17:36:35 +0200

    fix svg handling

--- .posthtmlrc.js
+++ .posthtmlrc.js
@@ -1,7 +1,16 @@
+const path = require('path')
+const fs = require('fs')
+const { promisify } = require('util')
+
+const readFile = promisify(fs.readFile)
+
+const root = __dirname + '/src'
+
 module.exports = {
   plugins: {
-    'posthtml-include': {
-      root: __dirname + '/src',
+    'posthtml-include': { root },
+    'posthtml-content': {
+      transclude: src => readFile(path.resolve(root, src), 'utf-8'),
     },
   },
 }
