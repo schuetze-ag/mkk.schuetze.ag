1b1084414ff08c7277e7201f2622f3f3b13e21c1
Philipp Jardas <philipp.jardas@schuetze.ag>
2019-04-10 16:40:52 +0200

    Animate facts numbers

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-import './styles/styles.scss';
+import $ from 'jquery';
 import 'bootstrap/js/src/modal.js';
+import './styles/styles.scss';
+
+window.jQuery = $;
+
+$(() => {
+  require('jquery-countto');
+  require('waypoints/lib/jquery.waypoints.js');
+
+  const facts = $('#facts');
+  const counters = facts.find('.js-counter');
+  counters.text('0');
+
+  facts.waypoint({
+    handler: function(direction) {
+      if (direction === 'down' && !facts.hasClass('animated')) {
+        console.log('starting facts animation');
+        facts.addClass('animated');
+        counters.countTo({
+          formatter: value =>
+            value.toLocaleString('de-DE', { maximumFractionDigits: 0 }),
+        });
+      }
+    },
+    offset: '80%',
+  });
+});
