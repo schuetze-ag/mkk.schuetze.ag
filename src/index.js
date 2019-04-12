cae6e1b8dd60aac002df4cae7a3a627e329d529b
Philipp Jardas <philipp.jardas@schuetze.ag>
2019-04-12 16:35:31 +0200

    Update styling and icons

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ $(() => {
   require('jquery-countto');
   require('waypoints/lib/jquery.waypoints.js');
 
+  // animate numbers
   const facts = $('#facts');
   const counters = facts.find('.js-counter');
   counters.text('0');
@@ -24,5 +25,21 @@ $(() => {
       }
     },
     offset: '80%',
+  });
+
+  // smooth scrolling for local anchor links
+  $('a[href*="#"]').on('click', function(e) {
+    e.preventDefault();
+    const href = $(this).attr('href');
+    const scrollTop = Math.max($(href).offset().top - 100, 0);
+    typeof history.pushState === 'function' &&
+      history.pushState(null, '', location.pathname + href);
+    $('html, body').animate({ scrollTop }, 500, 'swing');
+  });
+
+  // add box-shadow to nav bar
+  const body = $('body');
+  window.addEventListener('scroll', () => {
+    body[window.scrollY > 0 ? 'addClass' : 'removeClass']('scrolled');
   });
 });
